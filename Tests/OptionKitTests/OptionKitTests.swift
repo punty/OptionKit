e9ac9e808618852a7d89ccaeca480825b7fd9904
Francesco Puntillo <francesco.puntillo@gmail.com>
2016-09-24 00:40:53 +0800

    add failing test

--- Tests/OptionKitTests/OptionKitTests.swift
+++ Tests/OptionKitTests/OptionKitTests.swift
@@ -10,11 +10,16 @@ class OptionKitTests: XCTestCase {
         XCTAssert(res.options.count == 1)
         XCTAssert(res.extraArgs.count == 0)
     }
+    
+    func testFail() {
+        XCTAssert(false)
+    }
 
 
     static var allTests : [(String, (OptionKitTests) -> () throws -> Void)] {
         return [
             ("testSingleOptionParse", testSingleOptionParse),
+            ("testFail", testFail),
         ]
     }
 }
