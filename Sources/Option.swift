7da3cd27de96ca3bbb9e812bb1d3f22304a9b053
Francesco Puntillo <francesco.puntillo@ef.com>
2016-09-28 10:49:58 +0800

    use let for Option struct constant fields

--- Sources/Option.swift
+++ Sources/Option.swift
@@ -2,14 +2,14 @@
 import Foundation
 
 public struct Option: Hashable {
-    var shortName: String?
-    var name: String
-    var helpMessage:String
+    let shortName: String?
+    let name: String
+    let helpMessage:String
     var value: String?
-    var required: Bool
-    var takesArguments: Bool
+    let required: Bool
+    let takesArguments: Bool
     
-    public init(shortName: String, name: String, helpMessage: String, required: Bool, takesArguments: Bool) {
+    public init(shortName: String?, name: String, helpMessage: String, required: Bool, takesArguments: Bool) {
         value = nil
         self.takesArguments = takesArguments
         self.required = required
